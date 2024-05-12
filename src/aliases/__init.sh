40233e3b13af575179e831cd37587aa73e7d60cc
slegrand <ut5svsxgc@relay.firefox.com>
2024-05-12 17:05:13 -0400

    Add aliases functionality + crontab aliases

---
+++ src/aliases/__init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# @author   Sebastien LEGRAND
+# @license  MIT license
+#
+# @brief    Export all defined functions
+
+#----- begin
+
+# load aliases only if we are in interactive mode
+if [[ $- == *i* ]]; then
+
+  # loop over all the BASH scripts defined in the directory
+  for script in *.sh; do
+
+    # don't loop over ourself
+    if [[ "${script}" == "__init.sh" ]]; then
+      continue
+    fi
+
+    source ${script}
+  done
+
+fi
---
+++ src/aliases/crontab.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# @author   Sebastien LEGRAND
+# @license  MIT License
+#
+# @brief    Aliases for crontab management
+
+#----- globals
+CRONTAB_BIN=$(which crontab)
+
+#----- functions
+
+# we redefine a function crontab to ensure we cannot remove a crontab from production
+crontab() {
+  case "$1" in
+    "-r")
+      echo "Error: deletion of crontab is not authorized"
+      ;;
+    *)
+      command ${CRONTAB_BIN} $*
+      ;;
+  esac
+}
+
+#----- aliases
+alias cre="crontab -e"
+alias crl="crontab -l"
+alias crg="crontab -l | egrep -v '^#|^$' | grep -i"
+alias crs="crontab -l | egrep -v '^#|^$' | sort -n -k2 -k1"
