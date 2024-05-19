cdde1f580f5ce1022c97c5fb3b076e270cae8161
slegrand <ut5svsxgc@relay.firefox.com>
2024-05-19 10:46:45 -0400

    Create bash wrapper for KV store commands

---
+++ src/scripts/kvstore/__init.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# @author   Sebastien LEGRAND
+# @license  MIT License
+#
+# @brief    Bash Wrapper around the KVStore scripts
+
+#----- functions
+kvstore::main() {
+  . ${BSFPE_LIBRARY_DIR}/scripts/kvstore/kvstore.sh $*
+}
+
+kvstore::set() {
+  . ${BSFPE_LIBRARY_DIR}/scripts/kvstore/kvstore.sh set $*
+}
+
+kvstore::get() {
+  . ${BSFPE_LIBRARY_DIR}/scripts/kvstore/kvstore.sh get $*
+}
+
+kvstore::del() {
+  . ${BSFPE_LIBRARY_DIR}/scripts/kvstore/kvstore.sh del $*
+}
+
+kvstore::clean() {
+  . ${BSFPE_LIBRARY_DIR}/scripts/kvstore/kvstore.sh clean $*
+}
+
+kvstore::print() {
+  . ${BSFPE_LIBRARY_DIR}/scripts/kvstore/kvstore.sh print $*
+}
