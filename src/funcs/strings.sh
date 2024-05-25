36fc2b1641bb070a1c942ed4a679a3e72a10b706
slegrand <ut5svsxgc@relay.firefox.com>
2024-05-25 15:22:12 -0400

    Add several strings manipulation functions

--- src/funcs/strings.sh
+++ src/funcs/strings.sh
@@ -40,6 +40,7 @@ strings::trim() {
 }
 
 # convert a string to uppercase
+# arguments:
 #   $1 : the string
 # return:
 #   The same string, in uppercase
@@ -49,10 +50,65 @@ strings::to_upper() {
 }
 
 # convert a string to lowercase
+# arguments:
 #   $1 : the string
 # return:
 #   The same string, in lowercase
 strings::to_lower() {
   local __string="$*"
   echo "${__string,,}"
+}
+
+# return the length of a string
+# arguments:
+#   $1 : the string
+# return:
+# the length of the string
+strings::length() {
+  local __string="$*"
+  echo ${#__string}
+}
+
+# extract N characters from the left of a string
+# arguments:
+#   $1 : number of characters to extract
+#   $2 : the string
+# return:
+#   the extracted characters
+strings::left() {
+  local __count=$1
+  shift
+  local __string="$*"
+  echo ${__string:0:__count}
+}
+
+# extract N characters from the right of a string
+# arguments:
+#   $1 : number of characters to extract
+#   $2 : the string
+# return:
+#   the extracted characters
+strings::right() {
+  local __count=$1
+  shift
+  local __string="$*"
+  local __length=${#__string}
+  local __pos=$(( __length - __count ))
+  echo ${__string:__pos:__length}
+}
+
+# extract N characters from the string at the specified position
+# arguments:
+#   $1 : starting position
+#   $2 : length
+#   $* : the string
+# return:
+#   the extracted characters
+strings::mid() {
+  local __start=$1
+  local __length=$2
+  shift 2
+  local __string="$*"
+
+  echo ${__string:__start:__length}
 }
