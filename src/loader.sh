2e4d02488c152a06207b2b063488152fba081f12
slegrand <ut5svsxgc@relay.firefox.com>
2024-06-02 15:29:56 -0400

    loader: ShellCheck corrections

--- src/loader.sh
+++ src/loader.sh
@@ -9,28 +9,32 @@ set -uo pipefail
 
 #----- globals
 # export our location for all the scripts
-BSFPE_LIBRARY_DIR=$(dirname ${BASH_SOURCE})
+# shellcheck disable=SC2128
+BSFPE_LIBRARY_DIR=$(dirname "${BASH_SOURCE}")
 export BSFPE_LIBRARY_DIR
 
 
 #----- functions
 _pushd() {
-  pushd $1 >/dev/null 2>&1
+  # shellcheck disable=SC2164
+  pushd "$1" >/dev/null 2>&1
 }
 
 _popd() {
+  # shellcheck disable=SC2164
   popd >/dev/null 2>&1
 }
 
 #----- begin
 
 # ensure we are in the loader directory
-_pushd ${BSFPE_LIBRARY_DIR}
+_pushd "${BSFPE_LIBRARY_DIR}"
 
 # go through all the directories and load the functions
 for DIR in */; do
   if [[ -e ${DIR}/__init.sh ]]; then
-    _pushd ${DIR}
+    # shellcheck disable=SC2164
+    _pushd "${DIR}"
     source __init.sh
     _popd
   fi
