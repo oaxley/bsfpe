8e0a92ac9a2ac4d38a7a4020dfeaadee488f23d9
slegrand <ut5svsxgc@relay.firefox.com>
2024-07-12 21:36:13 -0400

    Add manpages information for docker::

--- distrib/lib/docker/container.sh
+++ distrib/lib/docker/container.sh
@@ -6,27 +6,64 @@
 # @brief    Docker Container specific functions
 
 #----- public functions
-# retrieve the logpath for a container
+
+#.--
+#.1 Retrieve the log path of the container
+#.2 (container){The name or ID of the docker container}
+#.3H Retrieve the log path of the container.
+#.4 Retrieve the log of container 'magical_wand'
+#.4 $ docker::log_path magical_wand
+#.4 Retrieve the log of contaner '0997655add3d'
+#.4 $ docker::log_path 0997655add3d
+#.--
 docker::log_path() {
   docker inspect --type container "$1" | jq -r '.[].LogPath'
 }
 
-# retrieve the name of the container
+#.--
+#.1 Retrieve the name of the container
+#.2 (container_id){The ID of the docker container}
+#.3H Retrieve the name of the docker container from its ID.
+#.4 Retrieve the name of contaner '0997655add3d'
+#.4 $ docker::get_name 0997655add3d
+#.--
 docker::get_name() {
   docker inspect --type container "$1" | jq -r '.[].Name' | cut -c2-
 }
 
-# retrieve the ID of a container
+#.--
+#.1 Retrieve the ID of the container
+#.2 (container){The name of the docker container}
+#.3H Retrieve the ID of the docker container from its name.
+#.4 Retrieve the ID of contaner 'magical_wand'
+#.4 $ docker::get_id magical_wand
+#.--
 docker::get_id() {
   docker inspect --type container "$1" | jq -r '.[].Id'
 }
 
-# retrieve the SHA256 base Image of a container
+#.--
+#.1 Retrieve the SHA256 of the container
+#.2 (container){The ID or name of the docker container}
+#.3H Retrieve the image ID of the docker container.
+#.3F This function returns the full SHA256 of the image. !!
+#.3F See \fBdocker::image_id_short\fR for the short SHA256.
+#.4 Retrieve the image ID of contaner 'magical_wand'
+#.4 $ docker::image_id magical_wand
+#.--
 docker::image_id() {
   docker inspect --type container "$1" | jq -r '.[].Image' | cut -d: -f2
 }
 
-# retrieve the short version of the SHA256
+#.--
+#.1 Retrieve the SHA256 (short) of the container
+#.2 (container){The ID or name of the docker container}
+#.3H Retrieve the image ID of the docker container.
+#.3F This function returns the short SHA256 (12 chars) of the image. !!
+#.3F See \fBdocker::image_id\fR for the full SHA256.
+#.4 Retrieve the image ID of contaner 'magical_wand'
+#.4 $ docker::image_id_short magical_wand
+#.--
 docker::image_id_short() {
   # shellcheck disable=SC2155
   local __image=$(docker::image_id "$1")
--- distrib/lib/docker/image.sh
+++ distrib/lib/docker/image.sh
@@ -6,26 +6,51 @@
 # @brief    Docker Container specific functions
 
 #----- public functions
-# retrieve the tag of a particular image
+
+#.--
+#.1 Retrieve the tag of an image
+#.2 (image_id){The ID of an image}
+#.3H Retrieve the full tag of the docker image.
+#.4 Retrieve the tag of image
+#.4 $ docker::image_tag 6e38f40d628d
+#.--
 docker::image_tag() {
   docker inspect --type image "$1" | jq -r '.[].RepoTags[0]'
 }
 
-# retrieve the image name
+#.--
+#.1 Retrieve the name of an image
+#.2 (image_id){The ID of an image}
+#.3H Retrieve the name of the docker image.
+#.4 Retrieve the name of an image
+#.4 $ docker::image_name 6e38f40d628d
+#.--
 docker::image_name() {
   # shellcheck disable=SC2155
   local __image_tag=$(docker::image_tag "$1")
   echo "${__image_tag}" | cut -d: -f1
 }
 
-# retrieve the image version
+#.--
+#.1 Retrieve the version of an image
+#.2 (image_id){The ID of an image}
+#.3H Retrieve the version of the docker image.
+#.4 Retrieve the version of an image
+#.4 $ docker::image_version 6e38f40d628d
+#.--
 docker::image_version() {
   # shellcheck disable=SC2155
   local __image_tag=$(docker::image_tag "$1")
   echo "${__image_tag}" | cut -d: -f2
 }
 
-# retrieve the image size
+#.--
+#.1 Retrieve the size of an image
+#.2 (image_id){The ID of an image}
+#.3H Retrieve the size in Bytes of the docker image.
+#.4 Retrieve the size of an image
+#.4 $ docker::image_size 6e38f40d628d
+#.--
 docker::image_size() {
   docker inspect --type image "$1" | jq -r '.[].Size'
 }
