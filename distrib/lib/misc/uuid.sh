17d6de801fcf978fe4c7433a450c159215ee34e9
slegrand <ut5svsxgc@relay.firefox.com>
2024-07-15 18:17:03 -0400

    Add manpages for uuid::

--- distrib/lib/misc/uuid.sh
+++ distrib/lib/misc/uuid.sh
@@ -12,9 +12,12 @@ __bin=$(which python3)
 
 #----- public functions
 
-# generate a UUID v4 number
-# return:
-#   a UUID v4 string
+#.--
+#.1 Generate a UUIDv4 number
+#.3H Generate a random UUIDv4 number.
+#.4 Generate a UUID
+#.4 $ uuid::uuid4     # output: 8b6f7afe-83a0-48a6-a8e1-846d85e06705
+#.--
 uuid::uuid4() {
   python3 -c 'import uuid; print(uuid.uuid4())'
 }
