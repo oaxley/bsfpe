0b093293cad52fcbed6ffebc77aaa559c144931d
slegrand <ut5svsxgc@relay.firefox.com>
2024-08-03 14:38:56 -0400

    Rewrite format::size function

--- distrib/lib/misc/format.sh
+++ distrib/lib/misc/format.sh
@@ -14,29 +14,11 @@ __bin=$(which bc)
 #.--
 #.1 Format a size value to human readable
 #.2 (value){the value in Bytes}
-#.3H Format a value in Bytes to its closest representable value. !!
-#.3F Supported values are:
-#.3F .br
-#.3F - \fBBytes\fR (B)!!
-#.3F - \fBKiloBytes\fR (KiB) !!
-#.3F - \fBMegaBytes\fR (MiB) !!
-#.3F - \fBGigaBytes\fR (GiB) !!
-#.3F - \fBTeraBytes\fR (TiB)!!
-#.4 Convert this size
+#.3H Format a value in Bytes to its closest representable value.
+#.3F Returns True (0) if successful, False (1) otherwise
+#.4 Convert this value to iec-i
 #.4 $ format::size 576345     # output: 562.836 KiB
 #.--
 format::size() {
-  local __value=$1
-  local __units=("B" "KiB" "MiB" "GiB" "TiB")
-  local __unit=0
-
-  # bash supports only integer
-  local __int_value=${__value/.*/}
-  while (( __int_value >= 1024 )); do
-    __unit=$(( __unit + 1 ))
-    __value=$(bc <<< "scale=3; ${__value} / 1024")
-    __int_value=${__value/.*/}
-  done
-
-  printf "%.003f %s\n" "${__value}" "${__units[${__unit}]}"
+  numfmt --to=iec-i --suffix="B" --format="%.02f" "$1"
 }
